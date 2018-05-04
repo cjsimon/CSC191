56d97e69cc970850adbce6963287ce1c333d07ab
lamphobic <kongregamer@gmail.com>
2018-05-04 14:10:57 -0700

    New table definitions

--- api/api/models/__init__.py
+++ api/api/models/__init__.py
@@ -11,11 +11,9 @@ class Models():
     # Import all models being used in the application here
     def init_models(self):
         from .user import init as User
-        from .stock import init as Stock
-        from .profile import init as Profile
-        from .paperportfolio import init as PaperPortfolio
+        from .owned import init as Owned
+        from .history import init as History
 
         User(self.Base)
-        Stock(self.Base)
-        Profile(self.Base)
-        PaperPortfolio(self.Base)
+        Owned(self.Base)
+        History(self.Base)
--- api/api/models/user.py
+++ api/api/models/user.py
@@ -2,13 +2,28 @@ from sqlalchemy import Column, Integer, String
 
 def init(Base):
     class User(Base):
-        __tablename__ = 'Users'
-        email = Column(String(120), unique=True, primary_key=True)
-        password = Column(String(120), unique=False)
+        __tablename__ = 'User'
+        uid = Column(Integer, ForeignKey("Users.uid"), primary_key=True)
+        username = Column(String(120), unique=False)
+        email = Column(String(120),  unique=True)
+        birthday = Column(String(8), unique=False)
+        phone = Column(String(50), unique=False)
+        sec1 = Column(String(120), unique=False)
+        sec2 = Column(String(120), unique=False)
+        sec3 = Column(String(120), unique=False)
+        ans1 = Column(String(120), unique=False)
+        ans2 = Column(String(120), unique=False)
+        ans3 = Column(String(120), unique=False)
 
-        def __init__(self, email=None, password=None):
+        def __init__(self, usrname=None, email=None, sec1=None, sec2=None, sec3=None, ans1=None, ans2=None, ans3=None):
+            self.username = username
             self.email = email
-            self.password = password
+            self.sec1 = sec1
+            self.sec2 = sec2
+            self.sec3 = sec3
+            self.ans1 = ans1
+            self.ans2 = ans2
+            self.ans3 = ans3
 
         def __repr__(self):
             return '<Email: %r\nPassword: %r>' % (self.email, self.password)
